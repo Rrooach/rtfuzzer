1d670dbbc34fa05c3576761c48277af302001f68
SunHao-0 <sunhao@outlook.com>
2020-09-22 11:37:48 +0000

    output diff to patches

--- diff.sh
+++ diff.sh
@@ -1,13 +1,13 @@
 #!/bin/bash 
 
-rm -rf ./patch > /dev/null 2>&1 
-mkdir patch 
+rm -rf ./patches > /dev/null 2>&1 
+mkdir patches 
 
 SYZ_DIR="../syzkaller"
 DIRS="syz-fuzzer syz-manager executor pkg prog" 
 
 for DIR in $DIRS
 do
-	diff -NrbBpu $SYZ_DIR/$DIR  ./$DIR > patch/$DIR.patch
+	diff -NrbBpu $SYZ_DIR/$DIR  ./$DIR > patches/$DIR.patch
 done
 
